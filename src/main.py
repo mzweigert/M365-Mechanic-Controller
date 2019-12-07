34c45135b7d9b5a8484912c45c5c0c20fba59f4b
mzweigert <mateuszzweigert@gmail.com>
2019-12-07 10:40:54 +0100

    Add progress bar to display module.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,22 @@
-from display import Display
+from display.base_display import BaseDisplay
+from display.progress_bar import ProgressBar
 from PIL import ImageFont
+
+from time import sleep
 
 if __name__ == '__main__':
     # Load a font in 2 different sizes.
-    font = ImageFont.truetype('/usr/share/fonts/truetype/orbitron/Orbitron-Bold.ttf', 20)
-    display = Display(font=font)
-    display.draw_text(0, 0, "Test", fill=1)
+    font = ImageFont.truetype('/usr/share/fonts/truetype/orbitron/Orbitron-Bold.ttf', 9)
+    display = BaseDisplay(font=font)
+    bar = ProgressBar(display, start_percent=0)
+    percent = 0
+    while percent <= 100:
+        percent += 1
+        bar.update(percent)
+        sleep(0.1)
+
+    percent = 100
+    while percent >= 0:
+        percent -= 1
+        bar.update(percent)
+        sleep(0.1)
