0c43541b920ce2f8dc0baf13df2d04255e838ec6
Mateusz.Zweigert <mateusz.zweigert@refinitiv.com>
2019-12-12 15:25:08 +0100

    Add font and hardware config scripts

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -20,30 +20,25 @@ install_python_module_if_not_exists() {
     fi
   done
 }
+
+install_python_module_with_different_names_if_not_exists() {
+    python3 -c "import $1"
+    if [ "$?" -eq 1 ]; then
+      sudo pip3 install "$2"
+    fi
+}
+
 # PROJECT MANAGEMENT
 install_apps_if_not_exists git python3-pip
 # GPIO
 install_apps_if_not_exists python3-rpi.gpio
 # OLED DISPLAY LIB
 install_apps_if_not_exists python3-dev python3-setuptools
-install_apps_if_not_exists libtiff4-dev libjpeg8-dev zlib1g-dev libfreetype6-dev liblcms1-dev libwebp-dev tcl8.5-dev tk8.5-dev
-install_python_module_if_not_exists Pillow adafruit_circuitpython_ssd1306
+install_apps_if_not_exists libjpeg8-dev zlib1g-dev libfreetype6-dev liblcms1-dev libwebp-dev tcl8.5-dev tk8.5-dev
+install_python_module_if_not_exists board
+install_python_module_with_different_names_if_not_exists PIL Pillow
+install_python_module_with_different_names_if_not_exists adafruit_ssd1306 adafruit-circuitpython-ssd1306
+
 # PROJECT LIBS
 install_python_module_if_not_exists click asyncio bleak
 
-if [ ! -d "$PROJECT_DIR/lib" ]; then
-    mkdir -p "$PROJECT_DIR/lib"
-fi
-
-command cd "$PROJECT_DIR"/lib
-
-if [ ! -d "py9b" ]; then
-  git clone https://github.com/mzweigert/py9b
-  command cd py9b
-  cp -r py9b "$PROJECT_DIR"/src/
-else
-  command cd py9b
-  git pull
-  cp -r py9b "$PROJECT_DIR"/src/
-  cd ..
-fi
